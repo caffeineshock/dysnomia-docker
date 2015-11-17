4311c334ef695db0986a1d70aab250425f9da4e7
Arne <caffeine@parttimegeeks.net>
2015-11-17 15:58:43 +0100

    Fix timezone and bundler performance

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM ruby:2.2.0
-ENV RAILS_ENV=development
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev
 RUN mkdir -p /var/www/dysnomia
 WORKDIR /var/www/dysnomia
 ADD ./source/Gemfile /var/www/dysnomia/Gemfile
 ADD ./source/Gemfile.lock /var/www/dysnomia/Gemfile.lock
+RUN bundle config --global jobs 4
 RUN bundle install
 ADD ./source /var/www/dysnomia
 RUN rake assets:precompile
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-git clone git@github.com:caffeineshock/dysnomia.git source
+
+FILE_MISSING=false
+
+check_config_file() {
+  CONFIG_FILE_PATH="source/config/$1"
+  if [ ! -f $CONFIG_FILE_PATH ]; then
+    echo -e "\033[1;31mConfigure\033[0m $CONFIG_FILE_PATH"
+    FILE_MISSING=true
+  fi
+}
+
+if [ ! -d "source" ]; then
+  git clone git@github.com:caffeineshock/dysnomia.git source
+fi
+
+check_config_file "database.yml"
+check_config_file "secrets.yml"
+check_config_file "private_pub.yml"
+
+if [ "$FILE_MISSING" = true ]; then
+  exit 1
+fi
+
 docker-compose build
+echo -e "\033[1;32mBuild complete! Run with\033[0m docker-compose up"
